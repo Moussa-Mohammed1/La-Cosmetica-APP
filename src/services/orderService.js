6110de8dc13f1a8bdb5dabddacb89a1d2d82e95b
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-03-31 21:50:10 +0100

    feat(service): enhance order service with additional functions for managing orders

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -5,8 +5,30 @@ export async function orderProductsService(products) {
 	return res?.data;
 }
 
+export async function getPendingOrder() {
+	const res = await api.get('/client/orders/pending');
+	return res?.data;
+}
 
+export async function addItem(payload) {
+	const res = await api.post('/order', {payload});
+	return res?.data
+}
+export async function getClientOrdersService(params) {
+	const res = await api.get('/orders');
+	return res?.data;
+}
+
+export async function getOrdersService() {
+	const res = await api.get('/client/orders');
+	return res?.data;
+}
 export async function cancelOrderService(id) {
 	const res = await api.put(`/orders/${id}/cancel`);
+	return res?.data;
+}
+
+export async function prepareOrderService(id) {
+	const res = await api.put(`/orders/${id}/prepare`);
 	return res?.data;
 }
