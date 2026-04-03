263a25de650261326fc017975f15d0ca7485962d
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-04-03 10:09:19 +0100

    feat(service): add product service functions for creating, updating, and deleting products

--- src/services/productService.js
+++ src/services/productService.js
@@ -10,3 +10,17 @@ export async function getProductDetails(slug) {
 	return res?.data;
 }
 
+export async function createProductService(payload) {
+    const res = await api.post('/products', payload);
+    return res?.data;
+}
+
+export async function updateProductService(id,payload) {
+    const res = await api.put(`/products/${id}`, payload);
+    return res?.data;
+}
+
+export async function deleteProductService(id) {
+    const res = await api.delete(`/products/${id}`);
+    return res?.data;
+}
